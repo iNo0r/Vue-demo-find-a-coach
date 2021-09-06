9c7a2f2525afc50d4074fa7e7382ddcc93d5cd8c
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-06 21:59:44 +0100

    establishing router

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp({}).mount('#app');
+import App from './App.vue';
+
+const app = createApp(App);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: null },
+    {
+      path: '/coaches/:id',
+      component: null,
+      children: [{ path: 'contact', component: null }] // /coaches/c1/contact
+    },
+    { path: '/register', component: null },
+    { path: '/requests', component: null },
+
+    { path: '/:catchAll(.*)', component: null }
+  ]
+});
+
+app.use(router);
+
+app.mount('#app');
