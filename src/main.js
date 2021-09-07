2c3f30a9c13aa6d4e97f0010df17349dd421b2bd
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-07 02:14:16 +0100

    0

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 
 import router from './router.js';
 
-import App from './App.vue';
+import App from './components/App.vue';
 
 const app = createApp(App);
 
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDeatil from './pages/coaches/CoachDetail.vue';
-import CoachList from './pages/coaches/CoachList.vue';
-import Coachregistration from './pages/coaches/Coachregistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestRecieved from './pages/requests/RequestReceived.vue';
-import NotFound from './pages/NotFound.vue';
+import CoachDeatil from './components/pages/coaches/CoachDetail.vue';
+import CoachList from './components/pages/coaches/CoachList.vue';
+import Coachregistration from './components/pages/coaches/Coachregistration.vue';
+import ContactCoach from './components/pages/requests/ContactCoach.vue';
+import RequestRecieved from './components/pages/requests/RequestReceived.vue';
+import NotFound from './components/pages/NotFound.vue';
 
 export default createRouter({
   history: createWebHistory(),
