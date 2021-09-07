3ffa7c1d4f2089c4693b2ef801de32b88537ebab
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-07 04:12:43 +0100

    base-card

--- src/components/pages/coaches/CoachList.vue
+++ src/components/pages/coaches/CoachList.vue
@@ -3,23 +3,25 @@
     FILTER
   </section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register"> Register as Coach</router-link>
-    </div>
+    <base-card>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register"> Register as Coach</router-link>
+      </div>
 
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No Coaches found.</h3>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No Coaches found.</h3></base-card
+    >
   </section>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/index.js';
 import App from './components/App.vue';
+import BaseCard from './components/layout/ui/BaseCard.vue';
+import BaseButton from './components/layout/ui/BaseButton.vue';
+import BaseBadge from './components/layout/ui/BaseBadge.vue';
 
 const app = createApp(App);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.use(router);
 app.use(store);
