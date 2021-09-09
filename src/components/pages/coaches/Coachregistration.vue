6bd4e4ab11ee2d333ff6948dd2ce4ab7303ddff5
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-09 04:02:59 +0100

    adding the coach to vuex

--- src/components/pages/coaches/Coachregistration.vue
+++ src/components/pages/coaches/Coachregistration.vue
@@ -4,7 +4,7 @@
       <h2>
         Register as a coach now!
       </h2>
-      <coach-form></coach-form>
+      <coach-form @save-data="saveData"></coach-form>
     </base-card>
   </div>
 </template>
@@ -14,6 +14,16 @@ import CoachForm from '../../coaches/CoachForm.vue';
 export default {
   components: {
     CoachForm
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      // this.$router.push('/coaches');
+
+      // the difference between router.push and router.replace, the replace will disallow the
+      // user to go back the previous page by going clicking backward
+      this.$router.replace('/coaches');
+    }
   }
 };
 </script>
