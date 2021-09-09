7008e88bbab1ac6b829eb76488db322fad945256
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-09 01:40:24 +0100

    binding the form with vue methods

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,39 +1,68 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="Firstname">Firstname</label>
-      <input type="text" id="firstname" />
+      <input type="text" id="firstname" v-model.trim="firstName" />
     </div>
     <div class="form-control">
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" />
+      <input type="text" id="lastname" v-model.trim="lastName" />
     </div>
     <div class="form-control">
       <label for="description">Description</label>
-      <textarea type="text" id="description"></textarea>
+      <textarea
+        type="text"
+        id="description"
+        v-model.trim="desctiption"
+      ></textarea>
     </div>
     <div class="form-control">
       <label for="rate">Hourly rate</label>
-      <input type="number" id="rate" />
+      <input type="number" id="rate" v-model.number="rate" />
     </div>
     <div class="form-control">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" />
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend" />
+        <input type="checkbox" id="backend" value="backend" v-model="areas" />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" />
+        <input type="checkbox" id="career" value="career" v-model="areas" />
         <label for="career">Career advisory</label>
       </div>
     </div>
     <base-button>Register</base-button>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: null,
+      areas: []
+    };
+  },
+  methods: {
+    submitForm() {
+      const formData = {
+        first: this.firstName,
+        last: this.lastName,
+        desc: this.description,
+        rate: this.rate,
+        areas: this.areas
+      };
+    }
+  }
+};
+</script>
 
 <style>
 /*  
