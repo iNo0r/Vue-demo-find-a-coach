f8509f459a96bde2619973e7be24a7374474416f
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-09 01:49:24 +0100

    styling the form

--- src/components/pages/coaches/Coachregistration.vue
+++ src/components/pages/coaches/Coachregistration.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    Coach registration
+    <base-card>
+      <h2>
+        Register as a coach now!
+      </h2>
+      <coach-form></coach-form>
+    </base-card>
   </div>
 </template>
+
+<script>
+import CoachForm from '../../coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm
+  }
+};
+</script>
