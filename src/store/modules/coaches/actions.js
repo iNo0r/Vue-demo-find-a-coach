7500c2d0208afcd0587127bb9967853bff1ce346
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-10 04:11:24 +0100

    registering the coach to firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,15 +1,39 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.getUserId;
     const coachData = {
-      id: context.rootGetters.getUserId,
       firstName: payload.first,
       lastName: payload.last,
       description: payload.desc,
       hourlyRate: payload.rate,
       areas: payload.areas
     };
-    context.commit('registerCoach', coachData);
+
+    // fetching and waiting the res
+    const response = await fetch(
+      // we need to add .json only with firbase
+      `https://coach-project-vue-vuex-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      // PUT will tell firebase to replace the data if it existed or just post it if not
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
+    // also waiting the res to get resolved
+    // const respondeData = await response.json();
+
+    if (!response.ok) {
+      //error..
+    }
+
+    context.commit('registerCoach', {
+      // the there dots will interpolate what is inside the object
+      ...coachData,
+      id: userId
+    });
   }
 };
 
-//how to acess a root getter from a store module
+//1- how to acess a rootgetter from a store module
+//2- using async await
+// - rather than using .then we can store the response utilizing the async method technique
