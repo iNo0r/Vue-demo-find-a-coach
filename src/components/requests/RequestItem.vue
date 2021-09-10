38d8700f7584b0f172542131fdb3b71a48384947
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-10 02:42:15 +0100

    outputing requests

---
+++ src/components/requests/RequestItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>
+    <div>
+      <a :href="emailLink"> {{ email }} </a>
+    </div>
+    <p>{{ message }}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['email', 'message'],
+  computed: {
+    emailLink() {
+      // this is special instruction, to direct you to an email client
+      return 'mailto:' + this.email;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/*  
+    1- what is the special syntax to direct the user to an email client
+
+
+ */
+</style>
