21274cbf742b1be1b45985f630d6b99fe621cf8b
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-11 06:38:05 +0100

    not found page

--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    not a valid route
-  </div>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        maybe you want to check our
+        <router-link to="/coaches">
+          main page.
+        </router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
